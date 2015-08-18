3bda3321d473ae47c5137671c60ce447e5a4c6c4
Andrew Haupt & Austin Kincaid <student@epicodus.com>
2015-08-18 11:55:28 -0700

    Add spec to test deletion of List instance

--- spec/to_do_integration_spec.rb
+++ spec/to_do_integration_spec.rb
@@ -10,18 +10,40 @@ describe("/lists", {:type => :feature}) do
     click_link("Create New List")
     fill_in('name', :with => "Epicodus work")
     click_button("Add List")
-    expect(page).to have_content("To Do List")
+    expect(page).to have_content("Epicodus work")
+  end
+
+  it("allows user to add task to a list") do
+    visit("/")
+    click_link("Create New List")
+    fill_in('name', :with => "Epicodus work")
+    click_button("Add List")
+    click_link("Epicodus work")
+    expect(page).to have_content("Create a new task")
+    fill_in('description', :with => 'Commit the details')
+    fill_in('due_date', :with => "2015-07-16")
+    click_button('Add Task')
+    expect(page).to have_content("Commit the details")
+  end
+
+  it("allows a user to delete a specific instance of List") do
+    visit("/")
+    click_link("Create New List")
+    fill_in('name', :with => "Epicodus work")
+    click_button("Add List")
+    click_link("X")
+    expect(page).to_not have_content("Epicodus work")
   end
 end
 
-describe("/tasks", {:type => :feature}) do
-  it("allows the user to add a new task") do
-    test_list = List.new({:name => "Chores", :id => nil})
-    test_list.save
-    visit("/lists/#{test_list.id}")
-    fill_in("description", :with => "Clean Room")
-    fill_in("due_date", :with => "2015-07-16")
-    click_button("Add Task")
-    expect(page).to have_content("Clean Room")
-  end
-end
+# describe("/tasks", {:type => :feature}) do
+#   it("allows the user to add a new task") do
+#     test_list = List.new({:name => "Chores", :id => nil})
+#     test_list.save
+#     visit("/lists/#{test_list.id}")
+#     fill_in("description", :with => "Clean Room")
+#     fill_in("due_date", :with => "2015-07-16")
+#     click_button("Add Task")
+#     expect(page).to have_content("Clean Room")
+#   end
+# end
